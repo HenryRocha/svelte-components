928e829f8f07902227ab27c2369d4754768dc969
Henry Rocha <henryrocha@protonmail.com>
2021-05-04 20:14:57 -0300

    Add center component

---
+++ src/components/Center/Center.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    /* Set to `true` to center vertically */
+    export let vertical: boolean = false;
+
+    /* Set to `true` to center horizontally */
+    export let horizontal: boolean = false;
+</script>
+
+<div
+    class:vertical-center={vertical && !horizontal}
+    class:horizontal-center={horizontal && !vertical}
+    class:full-center={horizontal && vertical}
+>
+    <slot />
+</div>
+
+<style>
+    .vertical-center {
+        @apply h-full flex flex-col justify-center;
+    }
+
+    .horizontal-center {
+        @apply flex flex-row justify-center;
+    }
+
+    .full-center {
+        @apply h-full flex flex-row justify-center items-center;
+    }
+</style>
---
+++ src/stories/Center.stories.svelte
@@ -0,0 +1,36 @@
+<script land="ts">
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Button from "../components/Button";
+    import Center from "../components/Center";
+</script>
+
+<Meta
+    title="Center"
+    component={Center}
+    argTypes={{
+        horizontal: {
+            control: {
+                type: "boolean"
+            }
+        },
+        vertical: {
+            control: {
+                type: "boolean"
+            }
+        }
+    }}
+/>
+
+<Template let:args>
+    <div class="bg-red-100 h-96 w-full">
+        <Center {...args}>
+            <Button>Button</Button>
+        </Center>
+    </div>
+</Template>
+
+<Story name="Horizontal" args={{ horizontal: true, vertical: false }} />
+
+<Story name="Vertical" args={{ horizontal: false, vertical: true }} />
+
+<Story name="Full" args={{ horizontal: true, vertical: true }} />
