1c5f48e094931e7a08d7f1c4addc5c19742494ef
Henry Rocha <henryrocha@protonmail.com>
2021-05-07 12:13:22 -0300

    Add version number component

---
+++ src/components/VersionNumber/VersionNumber.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    /**
+     * Specify the version number.
+     */
+
+    export let version: string = "dev-version";
+</script>
+
+<div class="version-number-container">
+    {#if version}
+        {version}
+    {:else}
+        Version not specified
+    {/if}
+</div>
+
+<style>
+    .version-number-container {
+        @apply absolute bottom-0 right-0 pr-2;
+        @apply text-sm text-right text-gray-500;
+    }
+</style>
---
+++ src/stories/VerisonNumber.stories.svelte
@@ -0,0 +1,20 @@
+<script land="ts">
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import VersionNumber from "../components/VersionNumber";
+</script>
+
+<Meta
+    title="VersionNumber"
+    component={VersionNumber}
+    argTypes={{
+        version: { control: "text" }
+    }}
+/>
+
+<Template let:args>
+    <VersionNumber {...args} />
+</Template>
+
+<Story name="Defined" args={{ version: "v1.0.0" }} />
+
+<Story name="Not defined" args={{}} />
